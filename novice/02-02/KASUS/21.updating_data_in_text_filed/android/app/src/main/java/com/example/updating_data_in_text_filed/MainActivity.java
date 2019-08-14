08e8f8b79e5a5734f6010cd9b98ab5bc2a20bf5b
okta <ermanjunaidi>
2019-08-14 11:51:53 +0700

    ketambahan lagi

---
+++ novice/02-02/KASUS/21.updating_data_in_text_filed/android/app/src/main/java/com/example/updating_data_in_text_filed/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.updating_data_in_text_filed;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
